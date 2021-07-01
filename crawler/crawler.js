9a1d0a819de934c24c41e432a21db6a931d52b64
Anna Lorimer <lemon@Annas-MacBook-Pro.local>
2021-07-01 13:10:01 -0400

    basic script to log requests on a defi site

--- crawler/crawler.js
+++ crawler/crawler.js
@@ -1,16 +1,69 @@
-const HCCrawler = require('headless-chrome-crawler');
+const csv = require('csv-parser');
+const fs = require('fs');
+const request_client = require('request-promise-native');
 
-(async () => {
-  const crawler = await HCCrawler.launch({
-    evaluatePage: () => ({
+// Read the list of sites to crawl  
+/*let sites = [];
+fs.createReadStream('sites.csv')
+  .pipe(csv())
+  .on('data', (row) => {
+  sites.push(row['site']);
+  console.log(row['site']);
+  })
+  .on('end', () => {
+    console.log('sites.csv processed');
+   });
+  console.log(sites);
+  evaluatePage: () => ({
       title: $('title').text(),
     }),
     onSuccess: result => {
       console.log(`Got ${result.result.title} for ${result.options.url}.`);
     },
   });
-  await crawler.queue('https://example.com/'); // Queue a request
-  await crawler.queue(['https://example.net/', { url: 'https://example.org/' }]); // Queue multiple requests in different styles
-  await crawler.onIdle();
-  await crawler.close();
+  
+*/
+ // Crawl the site
+
+(async () => {
+  const browser = await puppeteer.launch();
+  const page = await browser.newPage();
+  const result = [];
+
+  await page.setRequestInterception(true);
+
+  page.on('request', request => {
+    request_client({
+      uri: request.url(),
+      resolveWithFullResponse: true,
+    }).then(response => {
+      const request_url = request.url();
+      const request_headers = request.headers();
+      const request_post_data = request.postData();
+      const response_headers = response.headers;
+      const response_size = response_headers['content-length'];
+      const response_body = response.body;
+
+      result.push({
+        request_url,
+        request_headers,
+        request_post_data,
+        response_headers,
+        response_size,
+        response_body,
+      });
+
+      console.log(result);
+      request.continue();
+    }).catch(error => {
+      console.error(error);
+      request.abort();
+    });
+  });
+
+  await page.goto('https://app.sushi.com', {
+    waitUntil: 'networkidle0',
+  });
+
+  await browser.close();
 })();
