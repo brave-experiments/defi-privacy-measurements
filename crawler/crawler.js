0dc50933e8749887f0db58f00fb3280607e20e3d
Anna Lorimer <lemon@Annas-MacBook-Pro.local>
2021-07-02 15:06:45 -0400

    wip

--- crawler/crawler.js
+++ crawler/crawler.js
@@ -1,38 +1,14 @@
-const csv = require('csv-parser');
-const fs = require('fs');
+const HCCrawler = require('headless-chrome-crawler');
 const request_client = require('request-promise-native');
 
-// Read the list of sites to crawl  
-/*let sites = [];
-fs.createReadStream('sites.csv')
-  .pipe(csv())
-  .on('data', (row) => {
-  sites.push(row['site']);
-  console.log(row['site']);
-  })
-  .on('end', () => {
-    console.log('sites.csv processed');
-   });
-  console.log(sites);
-  evaluatePage: () => ({
-      title: $('title').text(),
-    }),
-    onSuccess: result => {
-      console.log(`Got ${result.result.title} for ${result.options.url}.`);
-    },
-  });
-  
-*/
- // Crawl the site
-
 (async () => {
-  const browser = await puppeteer.launch();
-  const page = await browser.newPage();
-  const result = [];
-
-  await page.setRequestInterception(true);
-
-  page.on('request', request => {
+  const crawler = await HCCrawler.launch({
+    maxDepth: 3,
+    customCrawl: async (page, crawl) => {
+      // You can access the page object before requests
+      const csv = [];
+      await page.setRequestInterception(true);
+      page.on('request', request => {
     request_client({
       uri: request.url(),
       resolveWithFullResponse: true,
@@ -44,7 +20,7 @@ fs.createReadStream('sites.csv')
       const response_size = response_headers['content-length'];
       const response_body = response.body;
 
-      result.push({
+      csv.push({
         request_url,
         request_headers,
         request_post_data,
@@ -53,17 +29,25 @@ fs.createReadStream('sites.csv')
         response_body,
       });
 
-      console.log(result);
+      console.log(csv);
       request.continue();
     }).catch(error => {
       console.error(error);
       request.abort();
     });
+  }); 
+      // The result contains options, links, cookies and etc.
+      const result = await crawl();
+      // You can access the page object after requests
+      result.content = await page.content();
+      // You need to extend and return the crawled result
+      return result;
+    },
+    onSuccess: result => {
+      console.log(`Got ${result.content} for ${result.options.url}.`);
+    },
   });
-
-  await page.goto('https://app.sushi.com', {
-    waitUntil: 'networkidle0',
-  });
-
-  await browser.close();
+  await crawler.queue('https://app.sushi.com/');
+  await crawler.onIdle();
+  await crawler.close();
 })();
